377de639fe2e43e094d96825d2e603e108645512
Shivam Singh <shivam003a@gmail.com>
2025-03-01 23:36:07 +0530

    add HashRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Toaster
         position='bottom-right' />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
